b04b3ea6e8fe5e568712dd66cc0101758a45ac70
Sean Takahashi <seanyuto@gmail.com>
2025-04-26 14:01:59 -0400

    initial commit

--- Driver.java
+++ Driver.java
@@ -1,7 +1,7 @@
 public class Driver {
     public static void main(String[] args) {
         if(args.length > 0){
-            if(args[0].equals("4")){
+            if(args[0].equals("1")){
                 if(args.length > 1){
                     if(args[1].equals("1")){
                         
@@ -14,7 +14,33 @@ public class Driver {
                     }
                 }
             }
-            else if(args[0].equals("5")){
+            if(args[0].equals("2")){
+                if(args.length > 1){
+                    if(args[1].equals("1")){
+                        
+                    }
+                    else if(args[1].equals("2")){
+
+                    }
+                    else if(args[1].equals("3")){
+
+                    }
+                }
+            }
+            if(args[0].equals("3")){
+                if(args.length > 1){
+                    if(args[1].equals("1")){
+                        
+                    }
+                    else if(args[1].equals("2")){
+
+                    }
+                    else if(args[1].equals("3")){
+
+                    }
+                }
+            }
+            else if(args[0].equals("4")){
                 if(args.length > 1){
                     if(args[1].equals("1")){
                         
