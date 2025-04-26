efcc3e940270e8e85cd17663dc4bf16c12657414
Sean Takahashi <seanyuto@gmail.com>
2025-04-26 14:33:19 -0400

    unaware of input format, changed to system.in

--- problem1/p1.java
+++ problem1/p1.java
@@ -1,26 +1,16 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class p1 {
-    public static int sol(String filename){
+    public static int sol(){
         String s = "";
         int count = 0;
-        try{
-            File file = new File(filename);
-            Scanner sc = new Scanner(file);
 
-            sc.nextLine();
-            s = sc.nextLine();
+        Scanner sc = new Scanner(System.in);
 
-            sc.close();
-        }catch(FileNotFoundException e){
-            System.out.println("File not found: " + filename);
-            return -1;
-        }catch(Exception e){
-            System.out.println("An error occurred: " + e.getMessage());
-            return -1;
-        }
+        sc.nextLine();
+        s = sc.nextLine();
+
+        sc.close();
 
         for(int i = 0; i < s.length(); i++){
             if(s.charAt(i) == 'p' || s.charAt(i) == 'c' || s.charAt(i) == 'l' || s.charAt(i) == 'a' || s.charAt(i) == 's' || s.charAt(i) == 'i' ){
@@ -32,16 +22,7 @@ public class p1 {
     }
 
     public static void main(String[] args) {
-        if(args.length > 0){
-            if(args[0].equals("1")){
-                boolean result = sol("input1.txt") == 3;
-                System.out.println("File 1: " + sol("input1.txt") + ", " + result);
-            }
-            if(args[0].equals("2")){
-                boolean result = sol("input2.txt") == 11;
-                System.out.println("File 2: " + sol("input2.txt") + ", " + result);
-            }
-        }
+        System.out.println(sol());
     }
 
 }
