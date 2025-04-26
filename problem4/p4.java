ee98188275f81ff529b532ec94612abc120d5da8
Sean Takahashi <seanyuto@gmail.com>
2025-04-26 16:40:44 -0400

    copied over some code and initial setup

--- problem4/p4.java
+++ problem4/p4.java
@@ -1,3 +1,31 @@
+import java.util.Scanner;
+
 public class p4 {
-    
+    public static int eval(String expression){
+        
+      }
+
+      public static int apply(int a, int b, String input){
+        if(input.equals("+")){
+          return a + b;
+        }
+        else if(input.equals("-")){
+          return b - a;
+        }
+        else if(input.equals("*")){
+          return a * b;
+        }
+        else if(input.equals("$")){
+          return a * b - a - b;
+        }
+        return -1;
+      }
+
+      public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+        String expression = sc.nextLine();
+        sc.close();
+        System.out.println(eval(expression));
+      }
 }
