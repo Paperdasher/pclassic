59275433c6048a4e0a8f2d4ffe3656df5f31a04b
Sean Takahashi <seanyuto@gmail.com>
2025-04-26 16:45:11 -0400

    made main able to take file input as well

--- problem4/p4.java
+++ problem4/p4.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class p4 {
     public static int eval(String expression){
-        
+        return -1;
       }
 
       public static int apply(int a, int b, String input){
@@ -22,10 +23,25 @@ public class p4 {
       }
 
       public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        sc.nextLine();
-        String expression = sc.nextLine();
-        sc.close();
-        System.out.println(eval(expression));
+        if(args.length > 0){
+            try{
+                File file = new File(args[0]);
+                Scanner sc = new Scanner(file);
+                sc.nextLine();
+                String expression = sc.nextLine();
+                sc.close();
+                System.out.println(eval(expression));
+            }catch(Exception e){
+                System.out.println("File not found or error reading file.");
+                return;
+            }
+
+        } else{
+            Scanner sc = new Scanner(System.in);
+            sc.nextLine();
+            String expression = sc.nextLine();
+            sc.close();
+            System.out.println(eval(expression));
+        }
       }
 }
