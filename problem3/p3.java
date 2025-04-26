24226bbc382ff09a09d7fe9c61a6a624ac2e4d95
Sean Takahashi <seanyuto@gmail.com>
2025-04-26 15:23:11 -0400

    skeleton setup

--- problem3/p3.java
+++ problem3/p3.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class p3 {
 
+    public static int sol(){
+        Scanner sc = new Scanner(System.in);
+
+
+        sc.close();
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(sol());
+    }
 }
