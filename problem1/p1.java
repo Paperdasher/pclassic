dd224816ab606a900910bae2bd08ed2df2e21145
Sean Takahashi <seanyuto@gmail.com>
2025-04-26 14:15:19 -0400

    separated into folders

---
+++ problem1/p1.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class p1 {
+    public static int sol(String filename){
+        String s = "";
+        int count = 0;
+        try{
+            File file = new File(filename);
+            Scanner sc = new Scanner(file);
+
+            sc.nextLine();
+            s = sc.nextLine();
+
+            sc.close();
+        }catch(FileNotFoundException e){
+            System.out.println("File not found: " + filename);
+            return -1;
+        }catch(Exception e){
+            System.out.println("An error occurred: " + e.getMessage());
+            return -1;
+        }
+
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'p' || s.charAt(i) == 'c' || s.charAt(i) == 'a' || s.charAt(i) == 's' || s.charAt(i) == 'i' || s.charAt(i) == 'l'){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
